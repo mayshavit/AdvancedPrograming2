4c08714959b916de047b99ff2d5c567a8d7ecc21
May <May@MAY-PC>
2017-05-22 17:18:35 +0300

    Windows and xaml

--- ex2/View/SettingsWindow.xaml.cs
+++ ex2/View/SettingsWindow.xaml.cs
@@ -27,5 +27,30 @@ namespace ex2
             vm = new SettingsViewModel(new ApplicationSettingsModel());
             this.DataContext = vm;
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Window window = Application.Current.MainWindow;
+            window.Close();
+        }
+
+        private void SwitchToMainWindow()
+        {
+            MainWindow win = (MainWindow)Application.Current.MainWindow;
+            win.Show();
+            //this.Close();
+            this.Hide();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            vm.SaveSettings();
+            SwitchToMainWindow();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchToMainWindow();
+        }
     }
 }
---
+++ ex2/View/SinglePlayerMenu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ex2
+{
+    /// <summary>
+    /// Interaction logic for SinglePlayerMenu.xaml
+    /// </summary>
+    public partial class SinglePlayerMenu : Window
+    {
+        public SinglePlayerMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Window window = Application.Current.MainWindow;
+            window.Close();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
